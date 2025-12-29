2c0beeb269fedfecd60e57b63b1b6b6862c85378
somyaknotfound <118343482+somyaknotfound@users.noreply.github.com>
2025-12-29 15:24:27 +0530

    Add sorting algorithm for 0s, 1s, and 2s
    Implement a solution to sort an array of 0s, 1s, and 2s.

---
+++ array/sortZeroOneTwo.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+#include <string.h>
+using namespace std;
+
+class Solution {
+    public:
+        void sortZeroOneTwo(vector <int> & nums) {
+            int czero = 0; int cone = 0 ; int ctwo = 0;
+            for (int i= 0 ; i < nums.size() ; i++) {
+                if (nums[i]==0) {
+                    czero++;
+                }
+                else if (nums[i]==1) {
+                    cone++;
+                    
+                }
+                else {
+                    ctwo++;
+                }
+                
+                
+            }
+            int index =  0;
+            
+            while (czero--) {
+                nums[index] = 0;
+                index++;
+            }
+            while (cone--) {
+                nums[index] = 1;
+                index++;
+            }
+            while (ctwo--) {
+                nums[index] = 2;
+                index++;
+            }
+            
+        }
+};
+
+int main() {
+  
+    
+    Solution sol;
+    
+    vector <int> zot = {0,2,1,1,1,2,2,2,0,0,0,2,1};
+    
+    sol.sortZeroOneTwo(zot);
+    
+    for (int num : zot) {
+        cout << num << " ";
+    }
+
+    
+    
+    return 0;
+}
