690ed452a547f20ce8bc4025ebb5a64c5b144469
somyaknotfound <118343482+somyaknotfound@users.noreply.github.com>
2026-01-02 23:59:24 +0530

    Add optimal max subarray sum function with output
    Implemented the optimal solution for finding the maximum subarray sum using Kadane's algorithm, including printing the subarray.

--- array/maxmSubarraySum.cpp
+++ array/maxmSubarraySum.cpp
@@ -69,6 +69,60 @@ class Solution{
         
         
         
+      
+    }
+
+    int MaxSubARrayOptimalPrint(vector <int> &nums) {
+        //kadane algorithm
+        
+        long long maxi = LLONG_MIN;
+        
+        long long sum = 0;
+        
+        int start = 0;
+        
+        int ansStart = -1 ;
+        int ansEnd = -1 ;
+        
+        
+        
+        for (int i =  0 ; i < nums.size();i++) {
+            
+            
+            if (sum == 0) {
+                start = i;
+            }
+            
+            sum += nums[i];
+            
+            if (sum > maxi) {
+                maxi = sum;
+                ansStart  = start;
+                ansEnd = i;
+            }
+            
+            if (sum < 0) {
+                sum = 0;
+                
+                
+            }
+        }
+        
+        
+        cout << "The Maximum Subarray is: [";
+        for (int i = ansStart ; i <= ansEnd ; i++) {
+            cout<< nums[i]<< ", ";
+        }
+        cout << "]"<<endl;
+        
+        
+        return maxi;
+        
+        
+        
+        
+        
+        
         
     }
     
