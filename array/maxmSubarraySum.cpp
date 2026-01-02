0e5cc5e2d64a4638b8f6ed9490300d968da50892
somyaknotfound <118343482+somyaknotfound@users.noreply.github.com>
2026-01-02 23:48:45 +0530

    Implement maximum subarray sum algorithms

---
+++ array/maxmSubarraySum.cpp
@@ -0,0 +1,99 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Solution{
+  public:
+    int MaxSubArrayBrute(vector  <int>&nums) {
+        int maxi  = INT_MIN;
+        
+        for (int i = 0 ; i < nums.size() ; i++)  {
+            for (int j = i ;  j < nums.size() ; j++) {
+                int sum = 0;
+                for (int k = i ; k <= j ; k++) {
+                    sum+=nums[k];
+                }
+                maxi = max(sum,maxi);
+            }
+            
+        }
+        
+        
+        return maxi;
+        
+        
+        
+    }
+    
+    int MaxSubArrayBetter(vector <int> &nums) {
+        
+        int maxi = INT_MIN;
+        
+        for(int i = 0 ;  i < nums.size() ; i++) {
+            int sum = 0;
+            for (int j = i ; j< nums.size() ; j++) {
+                sum+=nums[j];
+                
+                maxi = max(maxi,sum);
+            }
+        }
+        
+        
+        return maxi;
+    }
+    
+    
+    int MaxSubARrayOptimal(vector <int> &nums) {
+        //kadane algorithm
+        
+        long long maxi = LLONG_MIN;
+        
+        long long sum = 0;
+        
+        for (int i =  0 ; i < nums.size();i++) {
+            sum += nums[i];
+            
+            
+            if (sum > maxi) {
+                maxi = sum;
+            }
+            
+            if (sum < 0) {
+                sum = 0;
+                
+                
+            }
+        }
+        
+        return maxi;
+        
+        
+        
+        
+    }
+    
+    
+  
+  
+    
+};
+
+
+
+
+int main() {
+    
+    
+    Solution sol;
+    
+    
+    vector <int> arr = {-2,1,-3,4,-1,2,1,-5,4};
+    
+    int MaxSum = sol.MaxSubARrayOptimal(arr);
+    
+    
+    cout<< "Maximum contigious subarray sum "<<MaxSum;
+    
+    
+    return 0;
+}
