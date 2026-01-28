1305523d7f93f85105563098c3db07c19f4183f9
somyaknotfound <118343482+somyaknotfound@users.noreply.github.com>
2026-01-28 07:48:16 +0530

    Implement merge and mergeOptimal functions for intervals

--- array/mergeIntervals.cpp
+++ array/mergeIntervals.cpp
@@ -1,1 +1,64 @@
+#include <bits/stdc++.h>
 
+using namespace std;
+
+class Solution {
+    public:
+    vector <vector <int>> merge(vector <vector <int>> & intervals) {
+        sort(intervals.begin() , intervals.end());
+        
+        vector <vector<int>> ans;
+        
+        int n = intervals.size();
+        
+        for (int i = 0 ; i < n ;) {
+            int start = intervals[i][0];
+            int end = intervals[i][1];
+            
+            int j = i+1;
+            while (j < n && intervals[j][0] <= end) {
+                end = max(end , intervals[j][1]);
+                j++;
+                
+            }
+            
+            ans.push_back({start,end});
+            
+            i = j;
+        }
+        
+        return ans;
+    }
+    
+    vector <vector <int>> mergeOptimal(vector <vector <int>> & intervals) {
+        sort(intervals.begin()  ,intervals.end());
+        vector <vector <int>> merged;
+        
+        for (auto interval : intervals) {
+            if (merged.empty() || merged.back()[1] < interval[0]) {
+                merged.push_back(interval);
+            } else {
+                merged.back()[1] = max(merged.back()[1] , interval[1]);
+            }
+        }
+        
+        return merged;
+    }
+    
+    
+    
+    
+    
+};
+
+
+int main() {
+    Solution sol;
+    vector<vector<int>> intervals = {{1,3}, {2,6}, {8,10}, {15,18}};
+    vector<vector<int>> result = sol.mergeOptimal(intervals);
+
+    for (auto interval : result) {
+        cout << "[" << interval[0] << "," << interval[1] << "] ";
+    }
+    return 0;
+}
