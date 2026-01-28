ea8f7173db001db3022a675811ae4aaa89b32d30
somyaknotfound <118343482+somyaknotfound@users.noreply.github.com>
2026-01-28 14:41:13 +0530

    Implement canJump function for Jump Game

--- cp/JumpGame.cpp
+++ cp/JumpGame.cpp
@@ -25,4 +25,20 @@ public:
 
         
     }
+
+
+ 
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        int farthest = 0;
+        for (int i = 0 ; i < n ; i++) {
+            if (i> farthest) return false;
+            farthest = max(farthest , i + nums[i]);
+            if (farthest >= n-1) {
+                return true;
+            }
+        }
+        return farthest>=1;
+    }
+
 };
