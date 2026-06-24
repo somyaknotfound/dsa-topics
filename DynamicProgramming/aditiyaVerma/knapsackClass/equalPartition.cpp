9738987f28c12d3e467f8b9b59010ce4f18924a8
Somyak Mohanta <118343482+somyaknotfound@users.noreply.github.com>
2026-06-25 00:56:12 +0530

    Implement equal partition problem solution

---
+++ DynamicProgramming/aditiyaVerma/knapsackClass/equalPartition.cpp
@@ -0,0 +1,43 @@
+class Solution{
+public:
+    bool subsetSum(vector <int> &nums , int sum) {
+        int n = nums.size();
+        int dp[n+1][sum+1];
+
+        for (int i = 0 ; i < n +1 ; i++) {
+            for (int j = 0 ; j  < sum + 1 ; j++) {
+                if (i == 0) {
+                    dp[i][j] = false;
+                }
+
+                if (j == 0) {
+                    dp[i][j] = true;
+                }
+            }
+        }
+
+        for (int i = 1; i < n+1 ; i++) {
+            for (int j = 1 ; j < sum + 1; j++) {
+                if (nums[i-1] <= j) {
+                    dp[i][j] = dp[i-1][j-nums[i-1]] || dp[i-1][j];
+                } else {
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+
+        return dp[n][sum];
+    }
+
+
+    bool equalPartition(int n, vector<int> arr) {
+        int sum = accumulate(arr.begin() , arr.end() , 0);
+
+        if (sum%2!= 0) {
+            return false;
+        } else {
+
+        return subsetSum(arr,sum/2);
+        }
+    }
+};
