aeb475248ebff34897f6cf443ec06f90b50e17b3
Somyak Mohanta <118343482+somyaknotfound@users.noreply.github.com>
2026-07-06 21:47:48 +0530

    Add optimized next larger element function
    Implement optimized solution for finding next larger elements using a stack.

--- stack_queue/monotonic/nextGreaterEl.cpp
+++ stack_queue/monotonic/nextGreaterEl.cpp
@@ -29,3 +29,38 @@ public:
         return ans;
     }
 };
+
+
+
+
+class Solution {
+  public:
+    vector<int> nextLargerElementOpt(vector<int>& arr) {
+        // code here
+        stack <int> st;
+        int n = arr.size();
+        vector <int> ans(n);
+        
+        for (int i = n-1 ; i>= 0 ; i--) {
+            int curEl = arr[i];
+            
+            while (!st.empty() && st.top() <= curEl) {
+                st.pop();
+            }
+            
+            if (st.empty()) {
+                ans[i] = -1;
+            } else {
+                ans[i] = st.top();
+                
+                
+                
+            }
+            
+            st.push(curEl);
+        }
+        
+        return ans;
+        
+    }
+};
