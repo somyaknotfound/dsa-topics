27d2ef8d85788e53245fd3d9dd4d96fa536f1bb2
Somyak Mohanta <118343482+somyaknotfound@users.noreply.github.com>
2026-07-11 13:34:36 +0530

    Add CountEdges.cpp to count graph vertex degrees
    This program reads a graph's edges and counts the degree of each vertex.

---
+++ graphs/Basics/CountEdges.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+ 
+int main() {
+    
+    int n , m;
+    cin >> n >> m;
+    vector <int> graph[n+1];
+    
+    for (int i = 0 ; i < m ; ++i) {
+        int u, v;
+        cin>> u >> v;
+        graph[u].push_back(v);
+        graph[v].push_back(u);
+    }
+    
+    for (int i = 1  ; i <= n ; i++) {
+        cout << i << " : ";
+        int deg = 0;
+        for (int child : graph[i]) {
+            deg++;
+        }
+        cout << deg << endl;
+    }
+    
+    
+    
+    return 0;
+}
