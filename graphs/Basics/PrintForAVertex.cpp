1497b90ce8d71418b0237e2a97455ac3f7fe4fbf
Somyak Mohanta <118343482+somyaknotfound@users.noreply.github.com>
2026-07-11 13:42:32 +0530

    Add PrintForAVertex.cpp to display adjacent vertices
    Implement a program to print all adjacent vertices for a given vertex in a graph.

---
+++ graphs/Basics/PrintForAVertex.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+
+int main() {
+   int n , m;
+   cin>> n>>m;
+   
+   vector <int> graph[n+1];
+   
+   for (int i = 0 ; i < m ; ++i) {
+       int u ,v;
+       cin>> u >> v;
+       graph[u].push_back(v);
+       graph[v].push_back(u);
+   }
+   int j;
+   cin >> j;
+   
+   for (int child : graph[j]) {
+       cout << child << " ";
+   }
+    
+}
