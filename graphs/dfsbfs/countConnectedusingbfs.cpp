4bcc2b5c77bcc1e3ae0249bf98dfb163de4ca25c
Somyak Mohanta <118343482+somyaknotfound@users.noreply.github.com>
2026-07-24 14:04:59 +0530

    Add BFS algorithm for counting connected components
    Implement BFS to count connected components in a graph.

---
+++ graphs/dfsbfs/countConnectedusingbfs.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+void bfs(int vertex , vector <vector <int>> graph , vector <bool> &vis) {
+    queue <int> q;
+    
+    vis[vertex] = true;
+    q.push(vertex);
+    
+    while (!q.empty()) {
+        int node = q.front();
+        q.pop();
+        
+        // cout << node << "->";
+        
+        for (int child : graph[node]) {
+            if (!vis[child]) {
+                vis[child] = true;
+                q.push(child);
+            }
+        }
+    }
+}
+
+
+
+int main() {
+    int n,m;
+    
+    cin>>n>>m;
+    vector <vector <int>> graphs(n+1);
+    
+    for (int i = 0 ; i < m ; ++i) {
+        int u , v;
+        cin>>u>>v;
+        graphs[u].push_back(v);
+        graphs[v].push_back(u);
+    }
+    
+    vector <bool> vis(n+1 , false);
+    int count = 0;
+    
+    for (int i = 1;  i <= n ; i++) {
+        if (!vis[i]) {
+            count++;
+            bfs(i , graphs , vis);
+        }
+        // cout << endl;
+        
+    }
+    
+    cout << count << endl;
+    
+    
+    return 0;
+    
+}
