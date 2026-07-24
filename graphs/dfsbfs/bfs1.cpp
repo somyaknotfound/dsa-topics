e39f3dddbb9d2898157da1e5e7d9899a306cba9b
Somyak Mohanta <118343482+somyaknotfound@users.noreply.github.com>
2026-07-24 13:46:33 +0530

    Implement BFS algorithm in bfs1.cpp

---
+++ graphs/dfsbfs/bfs1.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+void bfs(int vertex , vector <vector <int> graph , vector <bool> &vis) {
+    queue <int> q;
+    
+    vis[vertex] = true;
+    q.push(vertex);
+    
+    while (!q.empty()) {
+        int node = q.front();
+        q.pop();
+        
+        cout << node << " ";
+        
+        for (int child : graph[vertex]) {
+            if (!vis[child]) {
+                vis[child] = true;
+                q.push(child);
+            }
+        }
+    }
+}
+
+
+
+int main() {
+    int n,m;
+    
+    cin>>n>>m;
+    vector <vector <int>> graphs(n+1);
+    
+    for (int i = 0 ; i < n ; ++i) {
+        int u , v;
+        cin>>u>>v;
+        graphs[u].push_back(v);
+        graphs[v].push_back(u);
+    }
+    
+    vector <bool> vis(n+1 , false);
+    
+    bfs(1, graphs , vis);
+    
+    return 0;
+    
+}
