cd0f94d0a244771e643eec1a75910f1680bd62bc
Somyak Mohanta <118343482+somyaknotfound@users.noreply.github.com>
2026-08-02 13:06:25 +0530

    Add ShapeCalculator class with area methods
    Implemented a ShapeCalculator class with methods to calculate the area of a circle, rectangle, and trapezoid using method overloading.

---
+++ OOPS/Polymorphism.cpp
@@ -0,0 +1,68 @@
+//Your code goes here
+
+class ShapeCalculator {
+    public:
+    void area(int radius) {
+        double ans = 3.14* radius*radius;
+        cout << "Area of Circle : " << (int)ans <<endl;
+
+    }
+
+    void area(int length , int width) {
+        int ans = (length*width);
+        cout << "Area of Rectangle : "<< ans << endl;
+    }
+
+    void area(int base1 , int base2 , int height) {
+        double ans = ((base1+base2)*height)/2;
+        cout << "Area of Trapezoid : "<< (int)ans << endl;
+    }
+
+};
+
+
+
+
+//Please Do not change anything below, It is only for your reference.
+/*
+
+This is the driver code that will execute and demonstrate the functionality of your class `ShapeCalculator`.
+
+It creates a object of class `ShapeCalculator`.
+It then calls the area method with radius as an argument. The method calculates the area of Circle and prints it.
+It then calls the area method with length and width as arguments. The method calculates the area of Rectangle and prints it.
+It then calls the area method with base1, base2, height as arguments. The method calculates the area of Trapezoid and prints it.
+
+
+public class Main {
+
+    public static void main(String[] args) {
+    
+        Scanner scanner = new Scanner(System.in);
+        
+        int radius = scanner.nextInt();
+        int length = scanner.nextInt();
+        int width = scanner.nextInt();
+        int base1 = scanner.nextInt();
+        int base2 = scanner.nextInt();
+        int height = scanner.nextInt();
+
+        ShapeCalculator sc = new ShapeCalculator();
+        sc.area(radius);
+        sc.area(length, width);
+        sc.area(base1, base2, height);
+
+        scanner.close();
+    }
+}
+
+*/
+
+/*
+//Below are the output statements
+
+System.out.println("Area of Circle : " + result);
+System.out.println("Area of Rectangle : " + result);
+System.out.println("Area of Trapezoid : " + result);
+
+*/
